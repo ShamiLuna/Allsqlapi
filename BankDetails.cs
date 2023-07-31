6e14735a0192e2d7b5243fb577ccec04d4d551a3
ShamiLuna <140070726+ShamiLuna@users.noreply.github.com>
2023-07-31 10:05:04 +0530

    Add files via upload

---
+++ BankDetails.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Mo
+{
+    public class BankDetails
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double AccountNumber { get; set; }
+    }
+}
---
+++ sqlco.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Model
+{
+    public class student
+    {
+        public String? NAME { get; set; }
+        public long ROLLNO { get; set; }
+        public string? MOBILENO { get; set; }
+        public string? CITY { get; set; }
+        public string? email { get; set; }
+    }
+}
